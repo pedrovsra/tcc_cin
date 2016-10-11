7edf694b75d12eaec0c80f8083ecfa727f0f2153
Pedro Tiago Araújo <ptvsaraujo@gmail.com>
2016-10-11 14:02:30 -0300

    mudanças no main

--- src/main/java/com/pedro/Main.java
+++ src/main/java/com/pedro/Main.java
@@ -3,6 +3,8 @@ package com.pedro;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
 import com.google.api.client.auth.oauth2.BearerToken;
@@ -21,11 +23,17 @@ import com.google.api.client.json.JsonObjectParser;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.DataStoreFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.pedro.entities.Track;
 import com.pedro.entities.analysis_entities.AudioAnalysis;
+import com.pedro.entities.analysis_entities.Segment;
 
 // https://developers.google.com/api-client-library/java/google-oauth-java-client/
 
 public class Main {
+
+	private static String MUSIC_ID;
 
 	private static final File DATA_STORE_DIR = new File(System.getProperty("user.home"), ".store/spotify_sample");
 
@@ -41,6 +49,8 @@ public class Main {
 
 	private static final String AUTH_SERVER_URL = "https://accounts.spotify.com/authorize";
 
+	private static final String[] CHROMATIC_SCALE = { "C ", "C#", "D ", "D#", "E ", "F ", "F#", "G ", "G#", "A ", "A#"," B " };
+
 	private static Credential authorize() throws Exception {
 		OAuth2ClientCredentials.errorIfNotSpecified();
 
@@ -53,21 +63,67 @@ public class Main {
 		LocalServerReceiver receiver = new LocalServerReceiver.Builder().setHost(OAuth2ClientCredentials.DOMAIN)
 				.setPort(OAuth2ClientCredentials.PORT).build();
 		return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-
 	}
 
-	private static void run(HttpRequestFactory requestFactory) throws IOException {
-		SpotifyUrl url = new SpotifyUrl("https://api.spotify.com/v1/audio-analysis/6GyDY0yE47rfk8pcuKhioh");
-		HttpRequest request = requestFactory.buildGetRequest(url);
-		AudioAnalysis a = request.execute().parseAs(AudioAnalysis.class);
-		System.out.println("sera?");
-		// System.out.println(request);
+	private static void printPitches(AudioAnalysis aa) {
+		List<Segment> segments = aa.getSegments();
+		int size = segments.size();
+		System.out.println("número de segmentos: " + size);
+		for (int i = 0; i < 12; i++) {
+			System.out.print(CHROMATIC_SCALE[i] + ": ");
+			for (int j = 0; j < size; j++) {
+				System.out.print(segments.get(j).getPitches()[i] + "\t");
+			}
+			System.out.println("");
+		}
+	}
+
+	private static float[][] generateMatrix(AudioAnalysis aa) {
+		List<Segment> segments = aa.getSegments();
+		int size = segments.size();
+		float[][] matrix = new float[12][size];
+
+		for (int i = 0; i < 12; i++) {
+			for (int j = 0; j < size; j++) {
+				matrix[i][j] = segments.get(j).getPitches()[i];
+			}
+		}
+		return matrix;
+	}
+
+	private static void run(HttpRequestFactory requestFactory, String music_id) throws IOException {
+		HttpRequest request;
+		Gson gson = new GsonBuilder().create();
+		// pegando informações da musica
+		SpotifyUrl urlInfo = new SpotifyUrl("https://api.spotify.com/v1/tracks/" + music_id);
+		request = requestFactory.buildGetRequest(urlInfo);
+		Track t = gson.fromJson(request.execute().parseAsString(), Track.class);
+		System.out.println(
+				"Obtendo informações da música '" + t.getName() + "' de '" + t.getArtists().get(0).getName() + "'");
+
+		SpotifyUrl urlAnalysis = new SpotifyUrl("https://api.spotify.com/v1/audio-analysis/" + music_id);
+		request = requestFactory.buildGetRequest(urlAnalysis);
+
+		String response = request.execute().parseAsString();
+
+		AudioAnalysis au;
+
+		try {
+			au = gson.fromJson(response, AudioAnalysis.class);
+			// printPitches(au);
+
+			float[][] m = generateMatrix(au);
+		} catch (Exception e) {
+			System.out.println("Não foi possível gerar a matriz de valores a partir dessa música :(");
+		}
+
 	}
 
 	public static void main(String[] args) {
-
-		System.out.println("TESTE");
-
+		Scanner in = new Scanner(System.in);
+		System.out.println("Digite o ID da música a ser analisada:");
+		MUSIC_ID = in.nextLine();
+		in.close();
 		try {
 			DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
 			final Credential credential = authorize();
@@ -77,7 +133,7 @@ public class Main {
 					request.setParser(new JsonObjectParser(JSON_FACTORY));
 				}
 			});
-			run(requestFactory);
+			run(requestFactory, MUSIC_ID);
 			// Success!
 			return;
 		} catch (IOException e) {
